cacf48d7ceed69a6a80f2e1eb66b63b2e9a6b3b0
David Alecrim <dsalecrim@outlook.com>
2025-03-15 09:53:58 -0300

    feat: add concurrent requests

--- test/e2e_test.go
+++ test/e2e_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"log/slog"
 	"net"
+	"sync"
 	"testing"
 
 	"kafka-from-scratch/internal/message"
@@ -92,7 +93,7 @@ func TestE2E(t *testing.T) {
 		conn, err := net.Dial("tcp", "localhost:9093")
 		require.NoError(t, err)
 
-		for i := 0; i < 20; i++ {
+		for i := range 20 {
 			request := message.DefaultRequest{
 				MessageSize:       0,
 				RequestAPIKey:     1,
@@ -120,5 +121,49 @@ func TestE2E(t *testing.T) {
 			assert.NotNil(t, response.NumberOfAPIKeys)
 			assert.Equal(t, response.ErrorCode, int16(0))
 		}
+	})
+
+	t.Run("should support multiple concorrent requests", func(t *testing.T) {
+		var wg sync.WaitGroup
+
+		for i := range 20 {
+			wg.Add(1)
+
+			go func() {
+				defer wg.Done()
+				conn, err := net.Dial("tcp", "localhost:9093")
+				require.NoError(t, err)
+
+				request := message.DefaultRequest{
+					MessageSize:       0,
+					RequestAPIKey:     1,
+					RequestAPIVersion: 4,
+					CorrelationID:     int32(1000000 + i),
+				}
+
+				reqBytes, err := request.ToBytes()
+				require.NoError(t, err)
+
+				_, err = conn.Write(reqBytes)
+				require.NoError(t, err)
+
+				readBuf := make([]byte, 1024)
+				n, err := conn.Read(readBuf)
+				require.NoError(t, err)
+
+				response, err := message.NewEmptyResponse(readBuf[:n])
+				require.NoError(t, err)
+
+				assert.Equal(t, request.CorrelationID, response.CorrelationID)
+				assert.NotNil(t, response.APIKey)
+				assert.NotNil(t, response.APIMinNumber)
+				assert.NotNil(t, response.APIMaxNumber)
+				assert.NotNil(t, response.NumberOfAPIKeys)
+				assert.Equal(t, response.ErrorCode, int16(0))
+			}()
+
+		}
+
+		wg.Wait()
 	})
 }
